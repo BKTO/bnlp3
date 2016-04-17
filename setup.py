70adde520c61d45b4b7beb14f6f163abb727c13c
BKTO <hello@betterknowtheopposition.com>
2016-04-17 05:57:16 +0000

    Added setup files

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from distutils.core import setup
 setup(
   name = 'bnlp',
   packages = ['bnlp'],
-  version = '0.2',
+  package_dir = {'bnlp': 'bnlp'},
+  package_data = {'bnlp': ['bnlp/*']},
+  version = '0.3',
   description = 'Better Natural Language Processing, a collection of open source nlp methods used by Better Know The Opposition',
   author = 'Better Know The Opposition',
   author_email = 'hello@betterknowtheopposition.com',
